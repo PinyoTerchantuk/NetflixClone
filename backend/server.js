58f3605b11bff27da2d9c5984d05a063d28f1a24
Pinyo <111266429+PinyoTerchantuk@users.noreply.github.com>
2024-12-05 11:58:50 +0700

    Protecting Routes in Expressjs with JWT tokens(Middleware) if not authen can watch tv or any movies YOU MUST LOGIN AND GET COOKIES BEFORE OTHERWISE CANT FETCH TV AND MVIES AND CANT SEE THIS GOD NETFLIX

--- backend/server.js
+++ backend/server.js
@@ -6,16 +6,20 @@ import tvRoutes from "./routes/tv.route.js";
 
 import { ENV_VARS } from "./config/envVar.js"; //config port
 import {connectDB} from "./config/db.js";  // config port
+import cookieParser from "cookie-parser";
+import { protectRoute } from "./middleware/protectRoute.js";
 
 
 const app = express();
 const PORT = ENV_VARS.PORT
 
 app.use(express.json()) //will allow us to parse req.body object important dont code this cant get {email, password, username}
+app.use(cookieParser()); //"cookie-parser": "^1.4.7" pass cookie from request to use middleware protect route that must authen first*/
+
 
 app.use("/api/v1/auth",authRoutes);
-app.use("/api/v1/movie",movieRoutes);
-app.use("/api/v1/tv",tvRoutes);
+app.use("/api/v1/movie",protectRoute, movieRoutes); // if authen axamine by protectRoute(MIddleWare) and then go next to fetch mvRoutesได้
+app.use("/api/v1/tv",protectRoute, tvRoutes);
 
 app.listen(PORT, ()=>{
     console.log("Server started at http://localhost:" + PORT);
