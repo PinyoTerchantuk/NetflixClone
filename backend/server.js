a674e90d736d0d87c4081f464f52b01d7a76f7bf
Pinyo <111266429+PinyoTerchantuk@users.noreply.github.com>
2025-01-13 19:20:49 +0700

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,13 @@ import {connectDB} from "./config/db.js";  // config port
 import cookieParser from "cookie-parser";
 import { protectRoute } from "./middleware/protectRoute.js";
 
+import path from "path";
+
 
 const app = express();
-const PORT = ENV_VARS.PORT
+const PORT = ENV_VARS.PORT;
+
+const __dirname = path.resolve();
 
 app.use(express.json()) //will allow us to parse req.body object important dont code this cant get {email, password, username}
 app.use(cookieParser()); //"cookie-parser": "^1.4.7" pass cookie from request to use middleware protect route that must authen first*/
@@ -22,6 +26,14 @@ app.use("/api/v1/auth",authRoutes);
 app.use("/api/v1/movie",protectRoute, movieRoutes); // if authen axamine by protectRoute(MIddleWare) and then go next to fetch mvRoutesได้
 app.use("/api/v1/tv",protectRoute, tvRoutes);
 app.use("/api/v1/search",protectRoute, searchRoutes);
+
+if(ENV_VARS.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+    app.get("*",(req,res) => {
+        res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
+    })
+}
 
 app.listen(PORT, ()=>{
     console.log("Server started at http://localhost:" + PORT);
